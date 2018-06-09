675254a110a0458dd1fcb2de60776cae3cfa2e44
anshuman <dbzdbs123@gmail.com>
2018-06-09 11:03:47 +0000

    potter: remote unused camera wrapper changes
    This reverts commit 719ba5ad2fcf4d2add89349a366835ed6ef3c899.
    
    Revert "Restart camera server when hal3 prop changes"
    
    This reverts commit 614f1ad1c26461729b03027c43aab55ef46fb349.
    
    Revert "potter: Build camera wrapper"
    
    This reverts commit 77e08933a514e2e3757ad8a5460d47d33134d200.
    
    Revert "potter: camera: Add logging on camera_put_parameters"
    
    This reverts commit 3f0d6c07d71efc907127d8049156fc2f100c6386.
    
    Revert "potter: camera: Rename module info"
    
    This reverts commit cca5ac1ffda3b347b2cd03c0a945c69b1bf19136.
    
    Revert "potter: camera: Bring back Wait for service"
    
    This reverts commit 35ac56db9b0b3b843aef632e9f192d49ec5bb74b.
    
    Revert "potter: camera: close native handles"
    
    This reverts commit a7e210a8e797b8a33c604e4fc847285da4e13e46.
    
    Revert "potter: camera: store user pointer and pass it when needed"
    
    This reverts commit dc1dcc19dc0b4edb887dc251123afa2042a445c7.
    
    Revert "potter: camera: Implement release before close safeguard"
    
    This reverts commit 503def364088e797ac51a562c1b18e3a898efce0.
    
    Revert "potter: camera: Fix build for O"
    
    This reverts commit ffde7d59082482a4f828195106d7c25ae7b98cc6.
    
    Revert "potter: camera: Add wrapper"
    
    This reverts commit 2a44c12ad63a801604fb716888a2b81f61c12057.
    
    Change-Id: Ia52c3cd5b49d06ad6aa6e4ad9dff9e4a0f397470

--- device.mk
+++ device.mk
@@ -103,11 +103,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libbson \
     Snap \
+    camera.device@1.0-impl \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
     libshim_camera_hal \
-    libshims_camera \
-    camera.msm8953
+    libshims_camera
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/camera/msm8953_mot_potter_camera.xml:system/etc/camera/msm8953_mot_potter_camera.xml \
